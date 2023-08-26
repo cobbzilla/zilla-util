e8edae250a32b2d031504d629351ff851bde2b9d
Jonathan Cobb <jonathan@kyuss.org>
2023-08-26 06:48:50 -0400

    rename clock

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,8 +1,8 @@
-export type YuebingClock = {
+export type ZillaClock = {
     now: () => number;
 };
 
-export const DEFAULT_CLOCK: YuebingClock = {
+export const DEFAULT_CLOCK: ZillaClock = {
     now: () => Date.now(),
 };
 
@@ -12,7 +12,7 @@ export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 export const DEFAULT_NAP_CHECK = 1000;
 
 const _nap =
-    (resolve: (v?: unknown) => void, clock: YuebingClock, alarm: NapAlarm, ms: number, check: number) => async () => {
+    (resolve: (v?: unknown) => void, clock: ZillaClock, alarm: NapAlarm, ms: number, check: number) => async () => {
         try {
             const start = clock.now();
             while (!alarm.wake && clock.now() - start < ms) {
@@ -27,5 +27,5 @@ export type NapAlarm = {
     wake?: boolean;
 };
 
-export const nap = (clock: YuebingClock, alarm: NapAlarm, ms: number, check?: number) =>
+export const nap = (clock: ZillaClock, alarm: NapAlarm, ms: number, check?: number) =>
     new Promise((r) => _nap(r, clock, alarm, ms, check || DEFAULT_NAP_CHECK)());
