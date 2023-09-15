c95a2b3376643758f30505f630a0a86c7476d093
Jonathan Cobb <jonathan@kyuss.org>
2023-09-14 22:23:59 -0400

    more date formatting utilities

--- lib/esm/string.d.ts
+++ lib/esm/string.d.ts
@@ -3,4 +3,6 @@ export declare const uncapitalize: (s: string) => string;
 export declare const basefilename: (s: string) => string;
 export declare const basefilenameWithoutExt: (s: string) => string;
 export declare const date_as_yyyyMMdd: (date: Date) => string;
+export declare const date_as_yyyyMMddHHmmSS: (date: Date) => string;
 export declare const timestamp_as_yyyyMMdd: (timestamp: number) => string;
+export declare const timestamp_as_yyyyMMddHHmmSS: (timestamp: number) => string;
