7a2cb80e5066cf38fc1308ca350b930dc57a0d16
Jonathan Cobb <jonathan@kyuss.org>
2023-09-14 22:48:32 -0400

    update date formatting utilities

--- src/string.ts
+++ src/string.ts
@@ -37,7 +37,7 @@ export const date_as_yyyyMMdd_HHmmSS = (date: Date): string =>
     `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, "0")}${date
         .getDate()
         .toString()
-        .padStart(2, "0")}_${("" + date.getHours()).padStart(2, "0")}${("" + date.getMinutes()).padStart(2, "0")}${(
+        .padStart(2, "0")}-${("" + date.getHours()).padStart(2, "0")}${("" + date.getMinutes()).padStart(2, "0")}${(
         "" + date.getSeconds()
     ).padStart(2, "0")}`;
 
