38b55fd9d828ca468fc3d39043fce7ab94d7c7f8
Jonathan Cobb <jonathan@kyuss.org>
2023-12-29 14:54:39 -0500

    add pure js sha256

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,2 +1,3 @@
-export * from "./string.js";
 export * from "./sleep.js";
+export * from "./string.js";
+export declare const sha256: (s: unknown) => string;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export * from "./string.js";
+import Sha256 from "./sha256.js";
 export * from "./sleep.js";
+export * from "./string.js";
+
+export const sha256 = (s: unknown) => Sha256.hash(`${s}`);
