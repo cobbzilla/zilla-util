a1181ed93be6b742b4a7335d0cd884c9780388ee
Jonathan Cobb <jonathan@kyuss.org>
2024-01-08 19:00:44 -0500

    add cartesianProduct

--- lib/esm/array.d.ts
+++ lib/esm/array.d.ts
@@ -1,1 +1,2 @@
 export declare const arraysEqual: <T>(a: T[], b: T[]) => boolean;
+export declare const cartesianProduct: <T>(arr: T[][]) => T[][];
--- src/array.ts
+++ src/array.ts
@@ -2,3 +2,21 @@ export const arraysEqual = <T>(a: T[], b: T[]): boolean => {
     if (a.length !== b.length) return false;
     return a.every((val) => b.includes(val)) && b.every((val) => a.includes(val));
 };
+
+const _cartesianProduct = <T>(arr: T[][], index: number, sentence: T[], result: T[][]) => {
+    for (let i = 0; i < arr[index].length; i++) {
+        const newSentence = sentence.slice(); // To not mutate the original array
+        newSentence.push(arr[index][i]);
+        if (index < arr.length - 1) {
+            _cartesianProduct(arr, index + 1, newSentence, result);
+        } else {
+            result.push(newSentence);
+        }
+    }
+};
+
+export const cartesianProduct = <T>(arr: T[][]): T[][] => {
+    const result: T[][] = [];
+    _cartesianProduct(arr, 0, [], result);
+    return result;
+};
