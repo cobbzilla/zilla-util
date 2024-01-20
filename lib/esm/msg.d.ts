174ab423b6b14176179d57c1b4889866f0ed7494
Jonathan Cobb <jonathan@kyuss.org>
2024-01-20 09:27:17 -0500

    Move more msg types from zilla-msg

--- lib/esm/msg.d.ts
+++ lib/esm/msg.d.ts
@@ -2,3 +2,25 @@ export type ZillaMsgTransportName = "email" | "sms";
 export declare const MESSAGE_TYPE_EMAIL: ZillaMsgTransportName;
 export declare const MESSAGE_TYPE_SMS: ZillaMsgTransportName;
 export declare const MESSAGE_TYPE_VALUES: ZillaMsgTransportName[];
+export type ZillaMsgRecipient = {
+    name?: string;
+    destination: string;
+};
+export type ZillaMessage = {
+    transport: ZillaMsgTransportName;
+    via: string;
+    template: string;
+    locale: string;
+    from: string;
+    to: string;
+    messages: Record<string, string>;
+    ctime: number;
+};
+export type ZillaMsgTransport = {
+    name: ZillaMsgTransportName;
+    sender: string;
+    via: () => string;
+    templates: string[];
+    formatTo: (recipient: ZillaMsgRecipient) => string;
+    send: (recipient: ZillaMsgRecipient, rendered: Record<string, string>) => Promise<unknown>;
+};
--- src/msg.ts
+++ src/msg.ts
@@ -3,3 +3,28 @@ export type ZillaMsgTransportName = "email" | "sms";
 export const MESSAGE_TYPE_EMAIL: ZillaMsgTransportName = "email";
 export const MESSAGE_TYPE_SMS: ZillaMsgTransportName = "sms";
 export const MESSAGE_TYPE_VALUES: ZillaMsgTransportName[] = [MESSAGE_TYPE_EMAIL, MESSAGE_TYPE_SMS];
+
+export type ZillaMsgRecipient = {
+    name?: string;
+    destination: string;
+};
+
+export type ZillaMessage = {
+    transport: ZillaMsgTransportName;
+    via: string;
+    template: string;
+    locale: string;
+    from: string;
+    to: string;
+    messages: Record<string, string>;
+    ctime: number;
+};
+
+export type ZillaMsgTransport = {
+    name: ZillaMsgTransportName;
+    sender: string;
+    via: () => string;
+    templates: string[];
+    formatTo: (recipient: ZillaMsgRecipient) => string;
+    send: (recipient: ZillaMsgRecipient, rendered: Record<string, string>) => Promise<unknown>;
+};
