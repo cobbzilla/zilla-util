66d90c9802e036956fa0a43862176d1e7dc56e0c
Jonathan Cobb <jonathan@kyuss.org>
2024-06-13 21:01:36 -0400

    update sleep name

--- lib/esm/time.d.ts
+++ lib/esm/time.d.ts
@@ -5,7 +5,7 @@ export type ZillaClock = {
 export declare const DEFAULT_CLOCK: ZillaClock;
 export type ZillaClockSource = () => ZillaClock;
 export declare const DEFAULT_CLOCK_SOURCE: ZillaClockSource;
-export declare const time: (ms: number) => Promise<unknown>;
+export declare const sleep: (ms: number) => Promise<unknown>;
 export declare const DEFAULT_NAP_CHECK = 1000;
 export type NapAlarm = {
     wake?: boolean;
--- src/time.ts
+++ src/time.ts
@@ -15,7 +15,7 @@ export type ZillaClockSource = () => ZillaClock;
 export const DEFAULT_CLOCK_SOURCE: ZillaClockSource = () => DEFAULT_CLOCK;
 
 // adapted from https://stackoverflow.com/a/39914235
-export const time = (ms: number) => new Promise((r) => setTimeout(r, ms));
+export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 export const DEFAULT_NAP_CHECK = 1000;
 
@@ -25,7 +25,7 @@ const _nap =
             alarm.wake = false;
             const start = clock.now();
             while (!alarm.wake && clock.now() - start < ms) {
-                await time(check);
+                await sleep(check);
             }
         } finally {
             resolve();
