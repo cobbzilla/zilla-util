b6ec26937109ef3083a7a33e17b76f5d57236e72
Jonathan Cobb <jonathan@kyuss.org>
2025-01-21 11:54:05 -0500

    export retry option defaults

--- lib/esm/retry.d.ts
+++ lib/esm/retry.d.ts
@@ -4,4 +4,9 @@ export type RetryOptions = {
     backoffMultiplier?: number;
     canRetry?: (e: Error) => boolean;
 };
+export declare const DEFAULT_RETRY_MAX_ATTEMPTS = 3;
+export declare const DEFAULT_RETRY_BACKOFF_BASE_MILLIS = 250;
+export declare const DEFAULT_RETRY_BACKOFF_MULTIPLIER = 3;
+export declare const DEFAULT_RETRY_CAN_RETRY_FUNC: () => boolean;
+export declare const DEFAULT_RETRY_OPTS: RetryOptions;
 export declare const retry: <T>(fn: () => Promise<T>, opts?: RetryOptions) => Promise<T>;
--- src/retry.ts
+++ src/retry.ts
@@ -6,10 +6,24 @@ export type RetryOptions = {
     backoffMultiplier?: number;
     canRetry?: (e: Error) => boolean;
 };
+
+export const DEFAULT_RETRY_MAX_ATTEMPTS = 3;
+export const DEFAULT_RETRY_BACKOFF_BASE_MILLIS = 250;
+export const DEFAULT_RETRY_BACKOFF_MULTIPLIER = 3;
+export const DEFAULT_RETRY_CAN_RETRY_FUNC = () => true;
+
+export const DEFAULT_RETRY_OPTS: RetryOptions = {
+    maxAttempts: DEFAULT_RETRY_MAX_ATTEMPTS,
+    backoffBaseMillis: DEFAULT_RETRY_BACKOFF_BASE_MILLIS,
+    backoffMultiplier: DEFAULT_RETRY_BACKOFF_MULTIPLIER,
+    canRetry: DEFAULT_RETRY_CAN_RETRY_FUNC,
+};
+
 export const retry = async <T>(fn: () => Promise<T>, opts?: RetryOptions): Promise<T> => {
-    const maxAttempts = opts?.maxAttempts || 3;
-    const backoffBaseMillis = opts?.backoffBaseMillis || 250;
-    const backoffMultiplier = opts?.backoffMultiplier || 3;
+    const maxAttempts = opts?.maxAttempts || DEFAULT_RETRY_MAX_ATTEMPTS;
+    const backoffBaseMillis = opts?.backoffBaseMillis || DEFAULT_RETRY_BACKOFF_BASE_MILLIS;
+    const backoffMultiplier = opts?.backoffMultiplier || DEFAULT_RETRY_BACKOFF_MULTIPLIER;
+    const canRetry = opts?.canRetry || DEFAULT_RETRY_CAN_RETRY_FUNC;
     let backoffTime = backoffBaseMillis;
     let error;
     for (let i = 0; i < maxAttempts; i++) {
@@ -21,7 +35,7 @@ export const retry = async <T>(fn: () => Promise<T>, opts?: RetryOptions): Promi
             return await fn();
         } catch (e) {
             error = e;
-            if (typeof opts?.canRetry === "function" && !opts.canRetry(e as Error)) {
+            if (!canRetry(e as Error)) {
                 throw e;
             }
         }
