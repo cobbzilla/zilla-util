dff8cf458338b01af1059452cca4368bb7580eb8
Jonathan Cobb <jonathan@kyuss.org>
2025-04-16 21:41:20 -0400

    enumRecord can take init object or function. bump minor version

--- src/object.ts
+++ src/object.ts
@@ -10,9 +10,11 @@ export const firstKeyByValue = <K extends string, V>(obj: Record<K, V>, val: V):
 
 export const enumRecord = <E extends string | number | symbol, V>(
     e: Record<string, E>,
-    init: () => V,
+    init: V | (() => V)
 ): Record<E, V> => {
     const o = {} as Record<E, V>;
-    for (const k of Object.values(e) as E[]) o[k] = init();
+    for (const k of Object.values(e) as E[]) {
+        o[k] = typeof init === "function" ? (init as () => V)() : init;
+    }
     return o;
 };
